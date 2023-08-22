7ccdb050c6d8b01fb1aba9bd605288cc78a9147c
豪肥肥 <35292408+HowePa@users.noreply.github.com>
2023-08-22 18:24:16 +0800

    add todo

--- migrate-local.sh
+++ migrate-local.sh
@@ -255,3 +255,4 @@ echo "################## Update .gitmodules ##################"
 link_submodules $TAR_URL/$src_name $BRANCH
 
 # Todo: reset update and then pull and push
+# Todo: add ssh auth for pull and push
